b65572bda08d3e737e7af9e9e86de4a154b69766
Terence Martin <github@nurdz.com>
2022-01-26 23:42:49 -0800

    Fix the websocket indentation
    This doesn't have any semantic changes, but it was a little bit distressing
    to realize that the websocket file has the wrong indent, since for this
    project we have standardized on 2 character indent.

--- src/web/websocket.js
+++ src/web/websocket.js
@@ -14,31 +14,31 @@ const WebSocketWrapper = require("ws-wrapper");
  * is disconnected or errors out and triggering a manual connection, until the
  * socket is eventualy connected again. */
 function getWebSocket(hostname, port) {
-    // Construct the URL that we're going to be connecting to.
-    const url = `ws://${hostname}:${port}`;
+  // Construct the URL that we're going to be connecting to.
+  const url = `ws://${hostname}:${port}`;
 
-    // Wrap a native websocket in our wrapper library; this will immediately
-    // connect the socket.
-    const socket = new WebSocketWrapper(new WebSocket(url));
+  // Wrap a native websocket in our wrapper library; this will immediately
+  // connect the socket.
+  const socket = new WebSocketWrapper(new WebSocket(url));
 
-    // Purely for informational reasons, display connection information so that
-    // we can verify that things are working as expected.
-    socket.on("open", event => console.log(`connect: ${event.target.url}`));
+  // Purely for informational reasons, display connection information so that
+  // we can verify that things are working as expected.
+  socket.on("open", event => console.log(`connect: ${event.target.url}`));
 
-    // In the event that our socket disconnects, wait a few seconds and then try
-    // to re-establish the connection again.
-    socket.on('disconnect', (event, wasOpen) => {
-        console.log(`disconnect: ${event.target.url}`);
-        setTimeout(() => socket.bind(new WebSocket(url)), 5000);
-    });
+  // In the event that our socket disconnects, wait a few seconds and then try
+  // to re-establish the connection again.
+  socket.on('disconnect', (event, wasOpen) => {
+    console.log(`disconnect: ${event.target.url}`);
+    setTimeout(() => socket.bind(new WebSocket(url)), 5000);
+  });
 
-    // If any error is detected on the socket, disconnect it if it's currently
-    // connected; that will trigger a reconnect using the above logic.
-    socket.on("error", event => {
-        console.log(`error: ${event.target.url}`);
-        socket.disconnect();
-    });
-    return socket;
+  // If any error is detected on the socket, disconnect it if it's currently
+  // connected; that will trigger a reconnect using the above logic.
+  socket.on("error", event => {
+    console.log(`error: ${event.target.url}`);
+    socket.disconnect();
+  });
+  return socket;
 }
 
 
