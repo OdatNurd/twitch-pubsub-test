433c3c384f85c92b21ecb61849366aa98f239826
Terence Martin <github@nurdz.com>
2022-01-28 22:45:47 -0800

    Clarify and clean up the test panel
    This cleans up the page that's used for generating test messages to make
    it more robust and easier to use.
    
    The larger part of this change is that we have a list of users that we
    can pick from instead of a text field, and that allows us to gather the
    information on a user (name, display name and user id) that we need to
    fake up messages in a believable way on the back end.
    
    Additionally the messages that transmit are more "correct" than they
    were before by ensuring that you can't anonymously sub (for example) and
    other similar cleanups.

---
+++ src/web/test_users.js
@@ -0,0 +1,39 @@
+// =============================================================================
+
+/* Some sample users for us to test with.
+ *
+ * In this object the top level key is the display of the user, and the value
+ * is an object that contains that user's user name and display name. */
+const test_user_data = {
+  "Anonymous": {
+    userId: null,
+    userName: null,
+    displayName: null,
+  },
+  "56740791": {
+    userId: "56740791",
+    userName: "phutbot",
+    displayName: "PhutBot",
+  },
+  "499189939": {
+    userId: "499189939",
+    userName: "marisuemartin",
+    displayName: "marisuemartin",
+  },
+  "136337257": {
+    userId: "136337257",
+    userName: "valleydweller",
+    displayName: "valleyDweller",
+  },
+  "66586458": {
+    userId: "66586458",
+    userName: "odatnurd",
+    displayName: "OdatNurd",
+  },
+};
+
+
+// =============================================================================
+
+
+module.exports = test_user_data;
