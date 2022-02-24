3f2f458967bfa3b9aa90a62c1709c9a81d0b5b99
Terence Martin <github@nurdz.com>
2022-02-23 23:26:27 -0800

    Implement starting a giveaway from the UI
    This implements all of the code needed to have the user facing panel
    prompt you for a duration and then kick off a new giveaway using it.
    
    This leverages the existing code that finds in-progress giveaways at
    startup time by creating a new database record and then invoking that
    method to do the actual work of the giveaway starting.

--- src/server/giveaway.js
+++ src/server/giveaway.js
@@ -2,26 +2,119 @@
 
 const { sendSocketMessage } = require('./socket');
 
+const humanize = require("humanize-duration");
+
+
 /* The database record that represents the currently running giveaway, if any.
  *
  * At any given time there can be at most one giveaway running in the channel,
  * although all past giveaways are archived for data reasons. */
 let currentGiveaway = undefined;
 
+/* When a giveaway timer is running, this is the ID that can be used to cancel
+ * it if we no longer want it to be running. */
+let giveawayTimerID = undefined;
+
+/* The clock time the last time the countdown event timer ticked; when it's 0
+ * it means that the timer has never ticked before. Before we start a timer,
+ * this is always set to the current time. */
+let lastTickTime = 0;
+
+/* The clock time the last time that the state of a running giveaway was backed
+ * up to the database; when its 0 it means that there has never been a giveaway
+ * this run. Before we start a timer, this is always set to the current time. */
+let lastSyncTime = 0;
+
 
 // =============================================================================
 
 
-function startGiveaway(req, res) {
-  console.log('starting giveaway');
-  currentGiveaway = {
-    startTime: new Date(),
-    endTime: null,
-    elapsedTime: 7200000,
-    paused: false,
+/* Process one tick of the giveaway timer; this is used to keep the database
+ * updated with currently elapsed times on the giveaway as well as to mark when
+ * the giveaway is completed. */
+async function giveawayTimerTick(db) {
+  // Get the time for the current tick.
+  const thisTime = Date.now();
+
+  // Figure out the elapsed time since the last timer tick happened; this could
+  // be more or less than the actual amount of time that we requested.
+  const deltaTime = thisTime - lastTickTime;
+  lastTickTime = thisTime;
+
+  // Add the amount of elapsed time for this tick to the elapsed time in the
+  // giveaway.
+  currentGiveaway.elapsedTime += deltaTime;
+
+  // If the elapsed time has been reached, the giveaway is now ended.
+  if (currentGiveaway.elapsedTime >= currentGiveaway.duration) {
+    console.log('Giveaway: Giveaway has ended');
+
+    currentGiveaway.endTime = new Date();
+    await db.getModel('giveaways').update( { id: currentGiveaway.id }, currentGiveaway);
+
+    // This giveaway is now completed, so we should tell interested parties
+    // about it.
+    currentGiveaway = undefined;
+
+    sendSocketMessage('giveaway-info', {});
+    return
   }
 
-  sendSocketMessage('giveaway-info', currentGiveaway);
+  // Send a status update to anyone interested.
+  sendSocketMessage('giveaway-tick', currentGiveaway);
+
+  // Dump the current information to the database.
+  if (thisTime - lastSyncTime >= 10000) {
+    await db.getModel('giveaways').update( { id: currentGiveaway.id }, currentGiveaway);
+    lastSyncTime = thisTime;
+  }
+
+  // Figure when the next tick should happen, which is either a second from now
+  // OR however much time is left on the timer, whichever is smaller.
+  const nextTick = Math.min(1000, currentGiveaway.duration - currentGiveaway.elapsedTime);
+
+  // console.log(`last = ${deltaTime}, next = ${nextTick}`);
+
+  // Schedule a new call
+  giveawayTimerID = setTimeout(() => giveawayTimerTick(db), nextTick);
+}
+
+
+// =============================================================================
+
+
+/* Start a new giveaway, if one is not already running. The request expects a
+ * duration in milliseconds and the userID of the user who the giveaway is for.
+ *
+ * Once this is done, the same handling that would trigger whenever a user is
+ * authorized is triggered, which will actually start the giveaway running. */
+async function startGiveaway(db, req, res) {
+  // Pull the ripcord if somehow this gets called when there's already a
+  // giveaway in progress.
+  if (currentGiveaway !== undefined) {
+    return;
+  }
+
+  // Insert into the database a new giveaway for the user provided that is
+  // flagged to start at the current time and use the given duration; it starts
+  // as non-paused and can in theory be for any user and not necessarily the
+  // currently authorized one (if any).
+  console.log(`Giveaway: New giveaway for ${req.query.userId} (${humanize(req.query.duration)})`);
+  await db.getModel('giveaways').create({
+    userId: req.query.userId,
+    startTime: new Date(),
+    endTime: null,
+
+    duration: req.query.duration,
+    elapsedTime: 0,
+    paused: false,
+  });
+
+  // Lean on the code that knows how to restart a giveaway for the current user
+  // and get it to find the entry we just made, set everything up and send off
+  // the notice that the giveaway is running.
+  resumeCurrentGiveaway(db, req.query.userId, false);
+
   res.json({success: true});
 }
 
@@ -29,7 +122,7 @@ function startGiveaway(req, res) {
 // =============================================================================
 
 
-function pauseGiveaway(req, res) {
+function pauseGiveaway(db, req, res) {
   console.log('pausing giveaway');
   currentGiveaway = {
     startTime: new Date(),
@@ -46,7 +139,7 @@ function pauseGiveaway(req, res) {
 // =============================================================================
 
 
-function unpauseGiveaway(req, res) {
+function unpauseGiveaway(db, req, res) {
   console.log('resuming giveaway');
   currentGiveaway = {
     startTime: new Date(),
@@ -63,7 +156,7 @@ function unpauseGiveaway(req, res) {
 // =============================================================================
 
 
-function cancelGiveaway(req, res) {
+function cancelGiveaway(db, req, res) {
   console.log('cancelling giveaway');
   currentGiveaway = undefined
 
@@ -86,7 +179,7 @@ function cancelGiveaway(req, res) {
  *
  * The giveaway data is updated, but the timer doesn't actually start until the
  * user requests it via the controls in the panel. */
-async function resumeCurrentGiveaway(db, twitch) {
+async function resumeCurrentGiveaway(db, userId, autoPause) {
   // console.log('==================================');
   // console.log(new Date());
   // console.log(await db.getModel('giveaways').find({}));
@@ -95,7 +188,7 @@ async function resumeCurrentGiveaway(db, twitch) {
   // Order the giveaway entries by their start time and pluck the most recent
   // one started by this user from the list; if there is currently a giveaway
   // running, it would be this one.
-  let entry = await db.getModel('giveaways').findOne({ userId: twitch.userInfo.id }, {
+  let entry = await db.getModel('giveaways').findOne({ userId }, {
     order: ['startTime', 'desc'],
     limit: 1
   });
@@ -106,12 +199,26 @@ async function resumeCurrentGiveaway(db, twitch) {
     return;
   }
 
-  console.log('A giveaway is in progress; pausing it');
+  // Set up this giveaway as the current one.
   currentGiveaway = entry;
-  currentGiveaway.paused = true;
-  sendSocketMessage('giveaway-info', currentGiveaway);
 
-  await db.getModel('giveaways').update({ id: currentGiveaway.id }, currentGiveaway);
+  if (autoPause === true) {
+    console.log('Giveaway: giveaway is in progress; auto-pausing it');
+    currentGiveaway.paused = true;
+    await db.getModel('giveaways').update({ id: currentGiveaway.id }, currentGiveaway);
+  }
+
+  // If the current giveaway is not paused, then we need to set up a timer that
+  // will actually track the giveaway duration. This will happen if a new
+  // giveaway was just created for example.
+  if (currentGiveaway.paused !== true) {
+    // Set the current time as the last time that the timer ticked, and then
+    // kick off the timer.
+    lastTickTime = lastSyncTime = Date.now();
+    giveawayTimerTick(db)
+  }
+
+  sendSocketMessage('giveaway-info', currentGiveaway);
 }
 
 
@@ -121,7 +228,7 @@ async function resumeCurrentGiveaway(db, twitch) {
 /* This is invoked in response to the user logging out of the overlay; when
  * this happens we need to pause the current giveaway, make sure it's updated
  * in the database, and then reset back to our initial startup state. */
-async function suspendCurrentGiveaway(db, twitch) {
+async function suspendCurrentGiveaway(db) {
   // If there's not currently a giveaway, then we don't need to do anything.
   if (currentGiveaway === undefined) {
     return;
@@ -146,15 +253,15 @@ async function suspendCurrentGiveaway(db, twitch) {
  * figuring out at startup if there is a current giveaway as well as sending out
  * messages regarding giveaway events as they occur. */
 function setupGiveawayHandler(db, app, bridge) {
-  bridge.on('twitch-authorize', twitch => resumeCurrentGiveaway(db, twitch));
-  bridge.on('twitch-deauthorize', twitch => suspendCurrentGiveaway(db, twitch));
+  bridge.on('twitch-authorize', twitch => resumeCurrentGiveaway(db, twitch.userInfo.id, true));
+  bridge.on('twitch-deauthorize', twitch => suspendCurrentGiveaway(db));
 
   // Set up the routes that allow the controls in the main panel to manipulate
   // the current giveaway state.
-  app.get('/giveaway/start', (req, res) => startGiveaway(req, res));
-  app.get('/giveaway/pause', (req, res) => pauseGiveaway(req, res));
-  app.get('/giveaway/unpause', (req, res) => unpauseGiveaway(req, res));
-  app.get('/giveaway/cancel', (req, res) => cancelGiveaway(req, res));
+  app.get('/giveaway/start', (req, res) => startGiveaway(db, req, res));
+  app.get('/giveaway/pause', (req, res) => pauseGiveaway(db, req, res));
+  app.get('/giveaway/unpause', (req, res) => unpauseGiveaway(db, req, res));
+  app.get('/giveaway/cancel', (req, res) => cancelGiveaway(db, req, res));
 
   // Every time a new socket connects to the server, send it a message to tell
   // it the state of the current giveaway, if any.
