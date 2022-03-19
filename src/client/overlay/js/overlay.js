f63bdbf5df42560b93149ad67985181da5f0bfae
Terence Martin <github@nurdz.com>
2022-03-18 23:07:24 -0700

    Implement an initial giveaway timer overlay
    This is a WIP of a single overlay object that can be dragged around the
    screen; this one is just the countdown clock, though we eventually want
    more.
    
    More work needs to be done in order to tie this properly into the back
    end, which is coming next.

--- src/client/overlay/js/overlay.js
+++ src/client/overlay/js/overlay.js
@@ -4,6 +4,37 @@
 const getConfig = require('../../common/js/config');
 const { getWebSocket } = require('../../common/js/websocket');
 
+import { gsap } from 'gsap';
+import { Draggable } from 'gsap/Draggable';
+
+const humanize = require("humanize-duration").humanizer({
+  language: "shortEn",
+  languages: {
+    shortEn: {
+      y: () => "y",
+      mo: () => "mo",
+      w: () => "w",
+      d: () => "d",
+      h: () => "h",
+      m: () => "m",
+      s: () => "s",
+      ms: () => "ms",
+    },
+  },
+  round: true
+});
+
+
+// =============================================================================
+
+
+/* The div that contains the current countdown text. */
+const countdownTxt = document.getElementById('countdown-clock');
+
+/* The status of the currently active giveaway (if any); this tracks things like
+ * the duration and the elapsed time. */
+let currentGiveaway = undefined;
+
 
 // =============================================================================
 
@@ -25,30 +56,71 @@ function setupTextBox(authorized, username) {
 // =============================================================================
 
 
+/* This is a helper function that can be used in a Draggable dragEnd event
+ * handler, and will transmit an overlay drag event to the back end code to
+ * tell the back end where the user decided the element that was dragged should
+ * appear on the overlay. */
+function dragEnder(target, socket) {
+  // transform: translate3d(633px, 286px, 0px);
+  const props = gsap.getProperty(target)
+  socket.emit('overlay-drag', {
+    name: target.id,
+    x: props('x'),
+    y: props('y')
+  })
+}
+
+
+// =============================================================================
+
+
 /* Set up everything in the overlay. This initializes the state of everything,
  * ensures that we're connected to the back end socket server, and sets up the
  * appropriate handlers for knowing when key events occur. */
 async function setup() {
-  // At startup assume we're not authorized until we learn differently.
-  setupTextBox(false);
+  // Make sure that bundlers know that we're actually using this object, since
+  // it's otherwise masked and we don't want tree shaking to kick us in the
+  // jimmies.
+  gsap.registerPlugin(Draggable);
 
   // Get our configuration, and then use it to connect to the back end so that
   // we can communicate with it and get events.
   const config = await getConfig();
   const socket = getWebSocket(location.hostname, config.socketPort);
 
-  // When the Twitch authorization state changes, change the text in the overlay
-  // box.
-  socket.on("twitch-auth", data => {
-    setupTextBox(data.authorized, data.userName);
+  // Make the countdown text div draggable; when the drag ends, this will ship
+  // a message to the back end to tell it the final location so that it can be
+  // persisted.
+  Draggable.create(countdownTxt, {
+    bounds: document.getElementById('viewport'),
+    onDragEnd: function () { dragEnder(this.target, socket) }
   });
 
-  socket.on("twitch-bits", data => {
-    console.log(data);
+
+  // When the information on the current giveaway changes, take an action; this
+  // triggers when  a new giveaway starts, one ends, or the pause state changes.
+  socket.on("giveaway-info", data => {
+    // Set up our current giveaway to track the incoming data.
+    currentGiveaway = Object.keys(data).length === 0 ? undefined : data;
+
+    if (currentGiveaway !== undefined) {
+      gsap.to(countdownTxt, { opacity: 1, duration: 1 });
+    }
+
+    if (currentGiveaway === undefined) {
+      gsap.to(countdownTxt, { opacity: 0, duration: 1 })
+    }
+
+    if (data.paused) {
+      countdownTxt.classList.add('pause');
+    } else {
+      countdownTxt.classList.remove('pause');
+    }
   });
 
-  socket.on("twitch-sub", data => {
-    console.log(data);
+  // When the duration of the giveaway changes, update things.
+  socket.on("giveaway-tick", data => {
+    countdownTxt.innerText = `${humanize(data.duration - data.elapsedTime)} remaining`;
   });
 }
 
