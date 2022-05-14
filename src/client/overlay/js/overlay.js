2d1aa9c3d1c6329c72241149f1a5c5f415cf215b
Terence Martin <github@nurdz.com>
2022-05-13 22:44:28 -0700

    Finalize the initial animation for the overlay
    This change pulls in our test logic and applies it to the actual update
    data and stored leaderboard data (if any) so that we actually animate
    people entering and leaving the list, list items changing positions,
    and so on.
    
    This is the initial version; more factoring is needed for this code,
    and we want to clean up the animations somewhat as well.

--- src/client/overlay/js/overlay.js
+++ src/client/overlay/js/overlay.js
@@ -39,6 +39,13 @@ const countdownTxt = document.getElementById('countdown-clock');
 const gifterSubBox = document.getElementById('gifters-subs');
 const gifterBitsBox = document.getElementById('gifters-bits');
 
+/* The native width of the headers for each of the boxes as defined in the HTML;
+ * as the contents of the boxes change, the header needs to also change it's
+ * width. We want to make sure we never make it smaller than the native size of
+ * the text, or it will wrap in an unfortunate way. */
+let subGifterHeaderMinWidth = undefined;
+let bitGifterHeaderMinWidth = undefined;
+
 /* The divs that contain the actual list of subs and bits leaders in each of the
  * boards. This is an aliased element lookup from the main gifter boxes. */
 const subListBox = document.getElementById('sub-list');
@@ -60,14 +67,17 @@ const domParser = new DOMParser();
  * the duration and the elapsed time. */
 let currentGiveaway = undefined;
 
-/* The list of people that are on the bits leaderboard; when this is undefined,
- * there is not currently anyone in the bits list. */
+/* The list of people that are on the bits and subs leaderboards; when they are
+ * undefined, there is not currently anyone in that list. When defined, the list
+ * is an array of the update objects that make up that leaderboard, in the order
+ * in which the items appear there. */
 let bitsLeaders = undefined;
+let subsLeaders = undefined;
 
 /* This HTML is used to specify the default entry in a gifter box when it's
  * empty, so that people know they should gift. This also allows us to pick up
  * the dimensions of an item in the list. */
-const placeholderHtml = `<div class="gift-box">Gift now to take the lead!</div>`;
+const placeholderHtml = `<div class="gift-box" data-twitch-id="-1">Gift now to take the lead!</div>`;
 
 
 // =============================================================================
@@ -126,18 +136,21 @@ function divForGifter(gifter) {
  *
  * If the header or the child div is not present, then this silently does
  * nothing so as to not destroy the DOM or trigger errors. */
-function resizeGifterHeader(boxElement) {
+function resizeGifterHeader(boxElement, minWidth) {
   const contents = boxElement.querySelector('div');
   const header = boxElement.querySelector('h4');
+  const headerWidth = minWidth ?? header.getBoundingClientRect().width;
 
   if (contents !== null && header !== null) {
     const maxWidth = Array.from(contents.children).reduce((prev, cur) => {
       const width = cur.getBoundingClientRect().width;
       return (width > prev) ? width : prev;
-    }, 0);
+    }, headerWidth);
 
-    header.style.width = `${maxWidth}px`;
+    gsap.to(header, { width: maxWidth, duration: 0.25 });
   }
+
+  return headerWidth
 }
 
 
@@ -161,141 +174,200 @@ function getGiftElementSize(boxElement) {
 // =============================================================================
 
 
-/* Update the leaderboard of the given type using the items provided; new items
- * will be added to the element provides. */
-function updateLeaderboard(board, type, items) {
-  console.log(`updateLeaderboard(${type})`);
-  // For now, only worry about bits; once this works for bits we can easily
-  // make it more generic for subs, since the containers are the same.
-  if (type !== 'bits') {
-    return;
+/* Given a DOM node that is the parent leaderboard element container for one of
+ * the leaderboards and a userID, return back the <div> that wraps that user;
+ * null will be returned if there is no such user found; an error will also be
+ * displayed. */
+function divForUserId(parentLeaderboard, userId) {
+  const div = parentLeaderboard.querySelector(`div[data-twitch-id="${userId}"]`);
+  if (div === null) {
+    console.log(`error while updating leaderboard; unable to find div for ${userId}`);
   }
 
-  // Special case: The very first update we get on this leaderboard for which
-  // there are any items, we can just create new divs specifically for those
-  // users, in their order on the leaderboard, and then manually animate them
-  // into position.
-  if (bitsLeaders === undefined) {
-    // When we get an update that's empty and we don't already know about a
-    // list, of gifters, this is a courtesy update; we can ignore it as it has
-    // no direct bearing on us.
-    if (items.length === 0) {
+  return div;
+}
+
+
+// =============================================================================
+
+/* Update the leaderboard of the given type using the items provided; new items
+ * will be added to the element provides. */
+function updateLeaderboard(giftBox, board, headerWidth, dimensions, current_leaderboard, display_size, updated_data) {
+  console.log(`updateLeaderboard(${headerWidth})`);
+  // console.log(JSON.stringify(current_leaderboard, null, 2));
+
+  // Create a timeline that will contain all of the animations that we're going
+  // to be displaying as a part of the update. We make this paused and add in
+  // animations as needed, and then play everything on the way out.
+  const timeline = gsap.timeline({ paused: true, onComplete: () => resizeGifterHeader(giftBox, headerWidth) });
+
+  // If the current leaderboard is undefined, this is the first update that it's
+  // ever received; for our work below we need this to be valid.
+  current_leaderboard = current_leaderboard ||   [];
+
+  // Capture a set that represents the list of people that are currently
+  // visualized on the leaderboard; this could be empty if this is the first
+  // update of its type, so we need to guard against the list not being
+  // available.
+  const leaderboard = new Set(current_leaderboard.map(e => e.userId));
+
+  // Do the same for the list of users in the update; this version is always an
+  // array because we're going to need to spread it to make cloned sub-arrays.
+  const update_users = updated_data.map(e => e.userId);
+
+  // Create sub-arrays that are the people that are eligible to be on the
+  // leaderboard or not eligible, based on the display size we were given.
+  const eligible_partipants = [...update_users].splice(0, display_size);
+  const ineligible_partipants = new Set([...update_users].splice(display_size));
+
+  // Create a version of the update data that's addressable by looking up based
+  // on the userId.
+  const update_map = updated_data.reduce((table, user) => {
+      table[user.userId] = user;
+      return table;
+    }, {});
+
+  // =============================================================
+  // Step 0: Update scores for people currently on the leaderboard
+  // =============================================================
+  current_leaderboard.forEach(user => {
+    const new_score = update_map[user.userId].score;
+
+    // If the score for this user didn't change, then there's nothing that we
+    // have to do.
+    if (user.score === new_score) {
       return;
     }
 
-    // Store this as the initial list of bits leaders for the next run through.
-    bitsLeaders = items;
+    // Make sure the entry in the leaderboard is up to date with the new score.
+    user.score = new_score;
 
-    // Create a div for each of these items and then add them to the page in
-    // their sorted order.
-    const bits = items.map((gifter, idx) => {
-      const div = divForGifter(gifter);
-      div.style.top = idx * bitListDim.height + bitListDim.top;
-      return div;
-    });
-    board.replaceChildren(...bits);
-
-    // We changed the children in ths box, so we need to update the title.
-    resizeGifterHeader(gifterBitsBox);
-
-    // Bounce them in from the right, in a staggered fashion; the list is now
-    // visualized, we can leave.
-    gsap.from(bits, { opacity: 1, x: 1920, duration: .65, stagger: 0.05, ease: "elastic.out(2, 0.4)" });
-    return;
-  }
-
-  // We previously had a list of users in the leaderboard, and now we're getting
-  // a whole new update.
-  //
-  // This can potentially generate any number of animations that we need to run
-  // in sequence, so start by making a timeline to contain them all. We'll start
-  // it out paused so that we can build everything up before we start it
-  // running.
-  const timeline = gsap.timeline({ paused: true });
-
-  // Scan over the new items; for any user that appears in the leaderboard
-  // already that has a gift score for this board that's different than the
-  // previous one, animate the number changing as a first step.
-  items.forEach(g => {
     // Look for a div that represents this particular user
-    const div = board.querySelector(`div[data-twitch-id="${g.userId}"]`);
-    if (div !== null) {
-      // If we can pull the score and it's different than it was previously,
-      // add an animation for it to blur out and back in .
-      const score = div.querySelector('.score');
-      if (score !== null) {
-        if (parseInt(score.innerText, 10) !== g.score) {
-          const tl = gsap.timeline({ defaults: { duration: 0.2 }})
-            .to(score, { blur: 5, scale: 2.5, onComplete: () => score.innerText = g.score })
-            .to(score, { blur: 0, scale: 1 });
+    const div = divForUserId(board, user.userId);
+    if (div === null) {
+      return;
+    }
 
-          timeline.add(tl, 0);
-        }
-      }
+    // Grab the score element out and animate it changing; this will make it
+    // scale up while blurring, then alter the text to the new value before
+    // shrinking back down.
+    const score = div.querySelector('.score');
+    if (score !== null) {
+      const subTl = gsap.timeline({ defaults: { duration: 0.2 }})
+        .to(score, { blur: 5, scale: 2.5, onComplete: () => score.innerText = user.score })
+        .to(score, { blur: 0, scale: 1 });
+
+      timeline.add(subTl, 0);
     }
   });
 
-  // Start the timeline playing now
-  timeline.play();
-  return;
 
-  /* ============================================================== */
-  /* Below here,  it's all pooptown, population us                  */
-  /* ============================================================== */
+  // ====================================================================
+  // Step 1: Kick people out of the leaderboard that have lost their spot
+  // ====================================================================
+  const leaving = [...leaderboard].filter(x => ineligible_partipants.has(x));
+  // console.log('leaving => ', leaving);
 
-  // The list is updating to new contents. Currently this assumes that the list
-  // always has the same people in it, for expediency in testing. In any case,
-  // save this new list of bits leaders so we know for next time.
-  bitsLeaders = items;
+  const departures_tl = gsap.timeline({ defaults: { duration: .65, ease: "elastic.out(2, 0.4)" }})
+  leaving.forEach(userId => {
+    const div = divForUserId(board, userId);
+    if (div === null) {
+      return;
+    }
 
-  // If there is only 1 item in the list, there's not a lot of exciting fake
-  // animations that we need to run.
-  if (items.length < 1) {
-    return;
+    departures_tl.to(div, { opacity: 0, x: -100, onComplete: () => {
+      board.removeChild(div);
+    }});
+  });
+  timeline.add(departures_tl);
+
+
+  // =======================================================================
+  // Step 2: Rearrange people that will remain in the list to their new spot
+  // =======================================================================
+  const loiterers = [...eligible_partipants].filter(x => leaderboard.has(x));
+  // console.log('loiterers => ', loiterers);
+
+  // If there are any loiterers around, then we can rearrange them as needed
+  // so that they end up in the right spot.
+  //
+  // NOTE: This is going to capture the state of all children as they currently
+  //       exist, but since we might have kicked people out, it's going to
+  //       capture the state of children that are going away but are technically
+  //       still here. Does that matter? Maybe not, if we position things such
+  //       that they end up in the same place anyway?
+  if (loiterers.length !== 0) {
+    // Save the state of the current children.
+    const startState = Flip.getState(board.children);
+    let shifted = 0;
+
+    // For each person hanging around, figure out what their index is going to
+    // be, and also what their index currently is. If they're different, then
+    // they need to move; if they're the same, we do nada.
+    loiterers.forEach(userId => {
+      const cur_idx = current_leaderboard.findIndex(user => user.userId === userId);
+      const new_idx = eligible_partipants.indexOf(userId);
+
+      if (cur_idx !== new_idx) {
+        const div = divForUserId(board, userId);
+        if (div === null) {
+          return;
+        }
+
+        shifted++;
+        div.style.top = new_idx * dimensions.height + dimensions.top;
+      }
+    });
+
+    // If any children have shifted locations, then we can schedule the
+    // animation now.
+    if (shifted !== 0) {
+      timeline.add(Flip.from(startState))
+    }
   }
 
-  // Create a timeline onto which we can attach our tweens.
 
+  // =======================================================================
+  // Step 3: Add incoming new people into the leaderboard at the proper spot
+  // =======================================================================
+  const arrivals = [...eligible_partipants].filter(x => leaderboard.has(x) === false);
+  // console.log('arrivals => ', arrivals);
 
-  // TODO:
-  // If the number of items in the list is different, the new person won't show
-  // up; that's an artifact of our hacky testing here. This should of course
-  // take that into account.
-
-  // Save the current state of the children in this board; this gets us an object
-  // that contains the current position state.
-  const state = Flip.getState(board.children);
-  const firstIdx = 0;
-  const lastIdx = items.length - 1;
-
-  // Get the list of child nodes, and the top positions of the first and last
-  // items, then swap their tops so that they appear to change positions visibly.
-  const kids = board.children;
-  const t1 = kids[firstIdx].getBoundingClientRect().top;
-  const t2 = kids[lastIdx].getBoundingClientRect().top;
-  kids[firstIdx].style.top = t2 - t1;
-  kids[lastIdx].style.top = t1 - t2;
-
-  // Get GSap to animate the items from the saved state. This will apply offsets
-  // to the items as they currently exist to put them back where they started
-  // visibly, and then animate them to the position they're currently in.
-  timeline.add(Flip.from(state, {
-    duration: 1,
-    ease: "elastic.out(1.5, 2)",
-    // absolute: true,
-    onComplete: () => {
-      // When the animation is complete, reset the tops and swap the dom
-      // elements so they go into their native positions.
-      kids[firstIdx].style.top = 0;
-      kids[lastIdx].style.top = 0;
-
-      // Swap the dom element position now
-      board.replaceChildren(...Array.from(kids).reverse());
+  // If the list of arrivals is not empty but the leaderboard is, then this is
+  // the very first user that's being added to the leaderboard. If that's the
+  // case, we need to vanish away the placeholder.
+  if (arrivals.length !== 0 && current_leaderboard.length === 0) {
+    // Find the placeholder div in this board; if we find it, make it hide
+    // itself, and then remove it from the DOM.
+    const div = divForUserId(board, "-1");
+    if (div !== null) {
+      const subTl = gsap.timeline()
+        .to(div, { duration: 0.50, scale: 0.25, blur: 3, opacity: 0, onComplete: () => {
+          board.removeChild(div);
+        }});
+      timeline.add(subTl);
     }
-  }));
+  }
 
+  const arrivals_tl = gsap.timeline({ defaults: { duration: .65, ease: "elastic.out(2, 0.4)" }})
+  arrivals.forEach(userId => {
+    const idx = eligible_partipants.indexOf(userId);
+    const gifter = update_map[userId];
+
+    const div = divForGifter(gifter);
+    div.style.top = idx * dimensions.height + dimensions.top;
+    board.appendChild(div);
+    arrivals_tl.from(div, { opacity: 0, x: 500 });
+
+  });
+  timeline.add(arrivals_tl);
+
+  // All animation setup is done, so start the animation running.
   timeline.play();
-  return;
+
+  // Return the new leaderboard; this is just the top portion of the incoming
+  // update.
+  return updated_data.splice(0, display_size);
 }
 
 
@@ -303,7 +375,7 @@ function updateLeaderboard(board, type, items) {
 
 
 /* Set up the global GSAP options that we need in order to get our animations
- * and other poritions of things working.
+ * and other positions of things working.
  *
  * This includes registering libraries, registering animation plugins, and so
  * on. */
@@ -366,8 +438,8 @@ function setupGiftBoxes() {
   // the placeholder.
   subListBox.innerHTML = placeholderHtml;
   bitListBox.innerHTML = placeholderHtml;
-  resizeGifterHeader(gifterSubBox);
-  resizeGifterHeader(gifterBitsBox);
+  subGifterHeaderMinWidth = resizeGifterHeader(gifterSubBox);
+  bitGifterHeaderMinWidth = resizeGifterHeader(gifterBitsBox);
 
   // Capture the dimensions of the gift boxes that we just added, so that as we
   // need to generate animations we can position them the same as they will
@@ -446,11 +518,12 @@ async function setup() {
     currentGiveaway = Object.keys(data).length === 0 ? undefined : data;
     if (currentGiveaway === undefined) {
       bitsLeaders = undefined;
+      subsLeaders = undefined;
 
       bitListBox.innerHTML = placeholderHtml;
       subListBox.innerHTML = placeholderHtml;
-      resizeGifterHeader(gifterBitsBox);
-      resizeGifterHeader(gifterSubBox);
+      resizeGifterHeader(gifterBitsBox, subGifterHeaderMinWidth);
+      resizeGifterHeader(gifterSubBox, bitGifterHeaderMinWidth);
     }
 
     if (currentGiveaway !== undefined) {
@@ -486,12 +559,14 @@ async function setup() {
   });
 
   // Update the bits leaderboard when a new message comes in.
-  socket.on('leaderboard-bits-update', data =>
-      updateLeaderboard(bitListBox, 'bits', data.splice(0, config.bitsLeadersCount)));
+  socket.on('leaderboard-bits-update', data => {
+    bitsLeaders = updateLeaderboard(gifterBitsBox, bitListBox, bitGifterHeaderMinWidth, bitListDim, bitsLeaders, config.bitsLeadersCount, data);
+  });
 
   // Update the subs leaderboard when a new message comes in.
-  socket.on('leaderboard-subs-update', data =>
-      updateLeaderboard(subListBox, 'subs', data.splice(0, config.subsLeadersCount)));
+  socket.on('leaderboard-subs-update', data => {
+      subsLeaders = updateLeaderboard(gifterSubBox, subListBox, subGifterHeaderMinWidth, subListDim, subsLeaders, config.subsLeadersCount, data);
+  });
 }
 
 
